6d707310cc1608a51d58c2b5ac66affb269a8304
Jaiglissechef-i9100 <yaggiland@gmail.com>
2018-08-16 20:31:52 +0200

    Move sensors to device

--- product/vibrator.mk
+++ product/vibrator.mk
@@ -13,8 +13,7 @@
 # limitations under the License.
 #
 
-# Sensors
+# Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
     android.hardware.vibrator@1.0-service \
     android.hardware.vibrator@1.0-impl
